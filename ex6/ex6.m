416c9f07cf88b1f8212718b78afba581039df489
Avanti Bhandarkar <51116892+avantibhandarkar@users.noreply.github.com>
2020-07-04 20:51:14 +0530

    Update ex6.m

--- ex6/ex6.m
+++ ex6/ex6.m
@@ -2,7 +2,7 @@
 %  Exercise 6 | Support Vector Machines
 %
 %  Instructions
-%  ------------
+%  -------------
 %
 %  This file contains code that helps you get started on the
 %  exercise. You will need to complete the following functions:
